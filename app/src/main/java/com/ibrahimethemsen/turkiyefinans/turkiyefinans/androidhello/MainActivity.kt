fcf391e0dd6840bc67882c82792f98aaa13b5e1b
İbrahim Ethem Şen <ibrahim-ethem-50@hotmail.com>
2023-02-22 12:10:11 +0300

    Initial commit

---
+++ app/src/main/java/com/ibrahimethemsen/turkiyefinans/turkiyefinans/androidhello/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ibrahimethemsen.turkiyefinans.turkiyefinans.androidhello
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
