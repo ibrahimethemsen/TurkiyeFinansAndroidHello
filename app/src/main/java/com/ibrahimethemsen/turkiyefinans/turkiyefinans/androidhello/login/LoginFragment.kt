0b315f031d7b1d3d998d1d2b4b33989ec22381b1
İbrahim Ethem Şen <ibrahim-ethem-50@hotmail.com>
2023-02-22 16:27:39 +0300

    forgot password fragment completed register fragment completed login fragment clickable spanned string added

--- app/src/main/java/com/ibrahimethemsen/turkiyefinans/turkiyefinans/androidhello/login/LoginFragment.kt
+++ app/src/main/java/com/ibrahimethemsen/turkiyefinans/turkiyefinans/androidhello/login/LoginFragment.kt
@@ -1,10 +1,18 @@
 package com.ibrahimethemsen.turkiyefinans.turkiyefinans.androidhello.login
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.ibrahimethemsen.turkiyefinans.turkiyefinans.androidhello.R
 import com.ibrahimethemsen.turkiyefinans.turkiyefinans.androidhello.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -20,5 +28,38 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        clickableText(R.string.register,::toRegisterFragment,binding.loginToRegisterTv)
+        clickableText(R.string.forgot_password,::toForgotPasswordFragment,binding.loginForgotPassword)
+    }
+
+    private fun clickableText(
+        @StringRes resString : Int,
+        clickHighOrder : () -> Unit,
+        clickText : TextView
+    ){
+        val resourceString = getString(resString)
+        val spannable = SpannableString(resourceString)
+        val clickable : ClickableSpan = object : ClickableSpan(){
+            override fun onClick(widget: View) {
+                clickHighOrder.invoke()
+            }
+        }
+        spannable.setSpan(
+            clickable,
+            0,
+            resourceString.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        clickText.text = spannable
+        clickText.movementMethod = LinkMovementMethod.getInstance()
+    }
+
+    private fun toForgotPasswordFragment(){
+        val action = LoginFragmentDirections.actionLoginFragmentToRegisterFragment()
+        findNavController().navigate(action)
+    }
+    private fun toRegisterFragment(){
+        val action = LoginFragmentDirections.actionLoginFragmentToForgotPasswordFragment()
+        findNavController().navigate(action)
     }
 }
